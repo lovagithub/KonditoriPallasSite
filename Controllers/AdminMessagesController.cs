205a086e1f15e9c068ee43920ddb3eb20c2ebfab
lovagithub <lova.juhlin@outlook.com>
2025-10-27 21:46:15 +0100

    2710 med contact

---
+++ Controllers/AdminMessagesController.cs
@@ -0,0 +1,29 @@
+﻿using KonditoriPallasSite.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KonditoriPallasSite.Controllers
+{
+	[Authorize]
+	public class AdminMessagesController(ApplicationDbContext db) : Controller
+	{
+		public async Task<IActionResult> Index()
+		{
+			var list = await db.ContactMessages
+				.OrderByDescending(x => x.SubmittedAt)
+				.ToListAsync();
+			return View(list);
+		}
+
+		public async Task<IActionResult> Delete(int id)
+		{
+			var msg = await db.ContactMessages.FindAsync(id);
+			if (msg == null) return NotFound();
+			db.ContactMessages.Remove(msg);
+			await db.SaveChangesAsync();
+			TempData["Ok"] = "Meddelandet togs bort.";
+			return RedirectToAction(nameof(Index));
+		}
+	}
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,29 +1,43 @@
+using KonditoriPallasSite.Data;
+using KonditoriPallasSite.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KonditoriPallasSite.Controllers
 {
 	public class HomeController : Controller
 	{
-		// Startsidans action
-		public IActionResult Index()
-		{
-			return View();
-		}
+		private readonly ApplicationDbContext _db;
+		public HomeController(ApplicationDbContext db) => _db = db;
 
-		// Exempel på extra sidor (frivilligt)
-		public IActionResult About()
-		{
-			return View();
-		}
+		// STARTSIDA
+		public IActionResult Index() => View();
 
+		// OM OSS
+		public IActionResult About() => View();
+
+		// LUNCH
+		public IActionResult Lunch() => View();
+
+		// KONTAKT (GET) – visar formuläret med modell
+		[HttpGet]
 		public IActionResult Contact()
 		{
-			return View();
+			return View(new ContactMessage());
 		}
 
-		public IActionResult Lunch()
+		// KONTAKT (POST) – tar emot och sparar
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Contact(ContactMessage model)
 		{
-			return View();
+			if (!ModelState.IsValid)
+				return View(model);
+
+			_db.ContactMessages.Add(model);
+			await _db.SaveChangesAsync();
+
+			TempData["Ok"] = "Tack! Ditt meddelande har skickats. Vi hör av oss inom kort.";
+			return RedirectToAction(nameof(Contact));
 		}
 	}
 }
