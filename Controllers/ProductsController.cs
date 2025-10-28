c1804d5603c04d4c20836e5a56b0c022e76b376b
lovagithub <lova.juhlin@outlook.com>
2025-10-28 17:53:55 +0100

    2810 med kategorier i menyn

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -3,30 +3,40 @@ using KonditoriPallasSite.Data;
 using KonditoriPallasSite.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace KonditoriPallasSite.Controllers
 {
 	public class ProductsController : Controller
 	{
 		private readonly ApplicationDbContext _db;
+		public ProductsController(ApplicationDbContext db) => _db = db;
 
-		// Dependency injection av DbContext
-		public ProductsController(ApplicationDbContext db)
+		// READ: lista produkter (med valfri filtrering via ?category=...)
+		public async Task<IActionResult> Index(string? category)
 		{
-			_db = db;
-		}
+			var query = _db.Products.Include(p => p.Category).AsQueryable();
 
-		// READ: lista produkter
-		public async Task<IActionResult> Index()
-		{
-			var products = await _db.Products.Include(p => p.Category).ToListAsync();
+			if (!string.IsNullOrWhiteSpace(category))
+			{
+				query = query.Where(p => p.Category != null && p.Category.Name == category);
+				ViewBag.CategoryName = category; // för rubrik i vyn
+			}
+
+			var products = await query
+				.OrderBy(p => p.Category!.Name)
+				.ThenBy(p => p.Name)
+				.ToListAsync();
+
 			return View(products);
 		}
 
 		// CREATE (GET): visa formulär
 		public IActionResult Create()
 		{
-			ViewBag.Categories = _db.Categories; // enklare selektionslista i vyn
+			ViewBag.Categories = _db.Categories
+									.OrderBy(c => c.Name)
+									.ToList();
 			return View();
 		}
 
@@ -35,14 +45,15 @@ namespace KonditoriPallasSite.Controllers
 		[ValidateAntiForgeryToken]
 		public async Task<IActionResult> Create(Product product)
 		{
-			if (!ModelState.IsValid) return View(product);
+			if (!ModelState.IsValid)
+			{
+				ViewBag.Categories = _db.Categories.OrderBy(c => c.Name).ToList();
+				return View(product);
+			}
 
 			_db.Products.Add(product);
 			await _db.SaveChangesAsync();
 			return RedirectToAction(nameof(Index));
 		}
-
-		// Det går fint att implementera Edit/Delete också för extra poäng
 	}
 }
-
